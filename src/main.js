0c640c32b8a9df9c325c32e7a6d794a58ca2b020
sz-p <sz_p@outlook.com>
2020-04-18 10:24:32 +0800

    [feat]: enable vuex

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import Router from 'vue-router'
 import router from './router'
+import { store } from './vuex/store';
+
 Vue.config.productionTip = false
 
 Vue.use(Router)
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
